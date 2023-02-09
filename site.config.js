abb318f7b0bdbb82362ff708b86704a928e10513
JasmineZHONG <124828186+JasmineZHONG@users.noreply.github.com>
2023-02-09 02:06:40 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: 'morethanmin/morethan-log',
+      repo: 'Jasmine/Jasmine‘s log',
       'issue-term': 'og:title',
       label: '💬 Utterances',
     }
